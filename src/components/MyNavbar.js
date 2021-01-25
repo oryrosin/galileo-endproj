d60e0037f6f171f2b3ef98fb7ce84dc980edec5a
ory-r <oooory@gmail.com>
2021-01-25 15:37:03 +0200

    changed brand

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -7,7 +7,7 @@ function MyNavbar(props) {
 
     return (
         <Navbar bg="dark" variant="dark" expand="sm">
-            <Navbar.Brand variant="dark"><img src="https://i.pinimg.com/736x/00/5d/24/005d2473868e07481dc5fae29e5cd9e7.jpg" width="40px" /> גלילאו מערכת שכר </Navbar.Brand>
+            <Navbar.Brand variant="dark"><img src="brand.jpg" width="50px" /> גלילאו מערכת שכר </Navbar.Brand>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     {activeUser ? <Nav.Link href="#/entrance" title="Home Page"> <FaHome/> </Nav.Link> : null}
